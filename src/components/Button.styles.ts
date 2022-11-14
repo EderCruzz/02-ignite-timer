1fa5e28d8ece356d05b8400f96417823454beb04
Eder Cruz <eder.ales10@gmail.com>
2022-11-14 18:52:51 -0300

    ignite-timer

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,31 @@
+import styled, { css } from 'styled-components'
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'sucess'
+
+interface ButtonContainerProps {
+    variant: ButtonVariant
+}
+
+const ButtonVariants = {
+    primary: 'purple',
+    secondary: 'orange',
+    danger: 'red',
+    sucess: 'green'
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+    width: 100px;
+    height: 40px;
+    border-radius: 4px;
+    border: none;
+    margin: 8px;
+
+    background-color: ${props => props.theme['green-500']};
+    color: ${props => props.theme.white};
+
+    /* ${props => {
+        return css`
+        background-color: ${ButtonVariants[props.variant]}
+        `
+    }} */
+`
